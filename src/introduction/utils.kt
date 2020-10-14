cdd71d4e840b20ec1c88a99484205eecb13ede68
Администратор <slegionv@yandex.ru>
2020-10-14 17:26:47 +0700

    Paragraph 5. Code formatting.

--- src/introduction/utils.kt
+++ src/introduction/utils.kt
@@ -12,16 +12,16 @@ fun getSortedInputList(args: Array<String>): List<String> {
     return getInputList(args).sorted()
 }
 
-fun formatList(result_list:List<String>): String {
+fun formatList(result_list: List<String>): String {
     return result_list.joinToString(separator = "\n")
 }
 
 fun getMapKeyCount(list_words: List<String>): MutableMap<String, Int> {
     val map: MutableMap<String, Int> = mutableMapOf()
-    list_words.forEach{map[it] =  map.getOrDefault(it, 0) + 1}
+    list_words.forEach { map[it] = map.getOrDefault(it, 0) + 1 }
     return map
 }
 
 fun myPrintMap(map: MutableMap<String, Int>) {
-    map.forEach{println("${it.key} ${it.value}")}
+    map.forEach { println("${it.key} ${it.value}") }
 }
