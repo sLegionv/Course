17f25b0f214268f67da25aee204683490846dd95
Администратор <slegionv@yandex.ru>
2020-10-14 21:29:01 +0700

    Paragraph 6. Comments.

--- src/introduction/4.kt
+++ src/introduction/4.kt
@@ -2,6 +2,6 @@ package introduction
 
 fun main(args: Array<String>) {
     val words: List<String> = getSortedInputList(args)
-    val map: MutableMap<String, Int> = getMapKeyCount(words)
+    val map: MutableMap<String, Int> = getMapWordCount(words)
     myPrintMap(map)
 }
--- src/introduction/utils.kt
+++ src/introduction/utils.kt
@@ -1,21 +1,20 @@
 package introduction
 
 
-fun getInputList(args: Array<String>) = when {
+fun getInputList(args: Array<String>) = when {  // Обработка ввода
     args.isNotEmpty() -> args.toList()
     else -> readLine()!!.split(" ")
 }
 
-fun getSortedInputList(args: Array<String>) = getInputList(args).sorted()
+fun getSortedInputList(args: Array<String>) = getInputList(args).sorted()  // Обработка ввода с сортировкой
 
+fun formatList(result_list: List<String>) = result_list.joinToString(separator = "\n")  // Вывод массива
 
-fun formatList(result_list: List<String>) = result_list.joinToString(separator = "\n")
-
-
-fun getMapKeyCount(list_words: List<String>): MutableMap<String, Int> {
-    val map: MutableMap<String, Int> = mutableMapOf()
-    list_words.forEach { map[it] = map.getOrDefault(it, 0) + 1 }
-    return map
+// Словарь; ключ - слово, значение - количество слов
+fun getMapWordCount(list_words: List<String>): MutableMap<String, Int> {
+    val mapWordCount: MutableMap<String, Int> = mutableMapOf()
+    list_words.forEach { mapWordCount[it] = mapWordCount.getOrDefault(it, 0) + 1 }
+    return mapWordCount
 }
 
-fun myPrintMap(map: MutableMap<String, Int>) = map.forEach { println("${it.key} ${it.value}") }
+fun myPrintMap(map: MutableMap<String, Int>) = map.forEach { println("${it.key} ${it.value}") }  // Вывод словаря
