54ba2cd00f55a39a53d0340dc54fc7191ea69a7b
Администратор <slegionv@yandex.ru>
2020-10-19 14:48:07 +0700

    Paragraph 14. Update func getMapWordCount

--- src/introduction/utils.kt
+++ src/introduction/utils.kt
@@ -8,13 +8,9 @@ fun getInputList(args: Array<String>) = when {  // Обработка ввода
 
 fun getSortedInputList(args: Array<String>) = getInputList(args).sorted()  // Обработка ввода с сортировкой
 
-fun formatList(result_list: List<String>) = result_list.joinToString(separator = "\n")  // Вывод массива
+fun formatList(resultList: List<String>) = resultList.joinToString(separator = "\n")  // Вывод массива
 
 // Словарь; ключ - слово, значение - количество слов
-fun getMapWordCount(list_words: List<String>): MutableMap<String, Int> {
-    val mapWordCount: MutableMap<String, Int> = mutableMapOf()
-    list_words.forEach { mapWordCount[it] = mapWordCount.getOrDefault(it, 0) + 1 }
-    return mapWordCount
-}
+fun getMapWordCount(listWords: List<String>) = listWords.groupingBy { it }.eachCount().toMutableMap()
 
-fun myPrintMap(map: MutableMap<String, Int>) = map.forEach { println("${it.key} ${it.value}") }  // Вывод словаря
+fun myPrintMap(map: Map<String, Int>) = map.forEach { println("${it.key} ${it.value}") }  // Вывод словаря
